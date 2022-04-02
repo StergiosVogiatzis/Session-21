99dd5fb7cd710f69035b5937b4bdca08a58f024e
siderism <michaelsid4@gmail.com>
2022-04-02 16:46:45 +0300

    created PetReport Handler

--- PetShop/PetShop.Web/Handlers/PetReportHandler.cs
+++ PetShop/PetShop.Web/Handlers/PetReportHandler.cs
@@ -1,6 +1,24 @@
-﻿namespace PetShop.Web.Handlers
+﻿using PetShop.EF.Context;
+using PetShop.Model;
+
+namespace PetShop.Web.Handlers
 {
     public class PetReportHandler
     {
+        private readonly PetShopContext _context;
+        private readonly PetReport _petReport;
+
+        public PetReportHandler(PetShopContext context, PetReport petReport)
+        {
+            _context = context;
+            _petReport = petReport;
+        }
+
+        public int TotalSold()
+        {
+            int year = Int32.Parse(_petReport.Year);
+            int month = Int32.Parse(_petReport.Month);
+            return _context.Transactions.Where(t => t.Date.Year == year && t.Date.Month == month && t.Pet.AnimalType == _petReport.AnimalType).Count();
+        }
     }
 }
